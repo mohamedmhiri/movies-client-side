e66561bb16260f42e7ec80f9166bf4d9bfd7cbfd
mohamed <mohamed.mohamed.mhiri@gmail.com>
2017-05-06 02:51:31 +0100

    actors list

--- src/app/actor/actor-list/actor-list.component.ts
+++ src/app/actor/actor-list/actor-list.component.ts
@@ -1,15 +1,33 @@
+import { Actor } from './../../models/actor';
+import { ActorService } from './../../services/actor.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-actor-list',
   templateUrl: './actor-list.component.html',
-  styleUrls: ['./actor-list.component.css']
+  styleUrls: ['./actor-list.component.css'],
+  providers: [ActorService]
 })
 export class ActorListComponent implements OnInit {
-
-  constructor() { }
+  actors: Array<Actor> = []
+  constructor(private service: ActorService) { }
 
   ngOnInit() {
+    this.service
+    .getAll()
+    .subscribe(data=> {
+      this.actors = data
+      console.log(this.actors)
+    })
+  }
+
+  ngOnChanges() {
+    this.service
+    .getAll()
+    .subscribe(data=> {
+      this.actors = data
+      console.log(this.actors)
+    })
   }
 
 }
